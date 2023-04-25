617a79764054a5f38e17555959d072102b4ee0d6
Alishah Safdari <alishahsafdari@gmail.com>
2023-04-25 14:20:45 +0430

    Fix the linter error

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const authorName = document.querySelector('.author');
 const add = document.querySelector('.add');
 const container = document.querySelector('.contain');
 function removeBook(book) {
-    books = books.filter((b) => b !== book);
-    localStorage.setItem('books', JSON.stringify(books));
-    container.innerHTML = '';
+  books = books.filter((b) => b !== book);
+  localStorage.setItem('books', JSON.stringify(books));
+  container.innerHTML = '';
 }
 
 function display() {
-    const div = document.createElement('div');
-    div.classList.add('book-list');
-    div.innerHTML = '';
-    container.innerHTML = books.reduce((output, book) => (
-        `${output
-        }
+  const div = document.createElement('div');
+  div.classList.add('book-list');
+  div.innerHTML = '';
+  container.innerHTML = books.reduce((output, book) => (
+    `${output
+    }
           <div class="book-list">
           <span>${book.title}</span>
           <span>${book.writer}</span>
@@ -25,29 +25,29 @@ function display() {
           </div>
            
         `
-    ), '');
+  ), '');
 
-    const button = document.querySelectorAll('.button');
-    button.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-            removeBook(books[index]);
-            display(books[index]);
-        });
+  const button = document.querySelectorAll('.button');
+  button.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      removeBook(books[index]);
+      display(books[index]);
     });
+  });
 }
 
 function addBook(book) {
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-    display(book);
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  display(book);
 }
 
 add.addEventListener('click', () => {
-    const newBook = { title: bookName.value, writer: authorName.value };
-    addBook(newBook);
+  const newBook = { title: bookName.value, writer: authorName.value };
+  addBook(newBook);
 });
 
 window.onload = () => {
-    books = JSON.parse(localStorage.getItem('books')) || [];
-    display(books);
+  books = JSON.parse(localStorage.getItem('books')) || [];
+  display(books);
 };
