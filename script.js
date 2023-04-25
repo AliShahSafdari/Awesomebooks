18e32cb012500e3ec3afd770a06d9e7cfcd4c675
nass <cabliveer@gmail.com>
2023-04-25 14:20:38 +0200

    Update remove and add function to class method

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-let books = [
-];
+class Book {
+  constructor(title, author){
+    this.title = title;
+    this.author = author
+  }
+}
+class Store{
+  constructor(){
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+  }
+  addBook(book){
+    this.books.push(book)
+    localStorage.setItem('books', JSON.stringify(this.books));
+    display()
+  }
+
+  removeBook(book) {
+    this.books = this.books.filter((b) => b !== book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    container.innerHTML = '';
+  }
+
+}
 
 const bookName = document.querySelector('.book');
 const authorName = document.querySelector('.author');
 const add = document.querySelector('.add');
 const container = document.querySelector('.contain');
-function removeBook(book) {
-  books = books.filter((b) => b !== book);
-  localStorage.setItem('books', JSON.stringify(books));
-  container.innerHTML = '';
-}
+let store = new Store()
 
 function display() {
   const div = document.createElement('div');
   div.classList.add('book-list');
   div.innerHTML = '';
-  container.innerHTML = books.reduce((output, book) => (
+  container.innerHTML = store.books.reduce((output, book) => (
     `${output
     }
           <div class="book-list">
@@ -30,24 +47,17 @@ function display() {
   const button = document.querySelectorAll('.button');
   button.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-      removeBook(books[index]);
-      display(books[index]);
+      store.removeBook(store.books[index]);
+      display();
     });
   });
 }
 
-function addBook(book) {
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  display(book);
-}
-
 add.addEventListener('click', () => {
-  const newBook = { title: bookName.value, writer: authorName.value };
-  addBook(newBook);
+  const newBook = new Book(bookName.value, authorName.value);
+  store.addBook(newBook);
 });
 
 window.onload = () => {
-  books = JSON.parse(localStorage.getItem('books')) || [];
-  display(books);
+  display();
 };
