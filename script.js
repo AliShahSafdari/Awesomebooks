e1d379244f57925914a5b59ca6696860bfab8990
Alishah Safdari <alishahsafdari@gmail.com>
2023-04-26 18:40:27 +0430

    The contact is added

--- script.js
+++ script.js
@@ -82,7 +82,19 @@ function addNewBook() {
   return content1;
 }
 function contact() {
-  const content1 = '<h1>This is out contact page.</h1>';
+  const content1 = `<div class="contact-continar">
+  <h2>Contact information</h2>
+  <div class="contact-content">
+  <p>Do you have any questions or you just want to say "Hello"?<br>
+  You can reach out to us!
+  </p>
+  <ul>
+  <li> Our e-mail<a>alishahsafdari905@gmail.com</a></li>
+  <li>Our phone number:+93744716508 </li>
+  <li> Our address : Microverse Students</li>
+  </ul>
+  </div>
+  </div>`;
   return content1;
 }
 
