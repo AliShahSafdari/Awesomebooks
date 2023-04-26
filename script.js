d1018ecb93b8ce8be39951340d544909c9b37f2c
Alishah Safdari <alishahsafdari@gmail.com>
2023-04-26 13:32:15 +0430

    Implement three content section

--- script.js
+++ script.js
@@ -71,3 +71,49 @@ add.addEventListener('click', (e) => {
 window.onload = () => {
   store.display();
 };
+
+
+
+// function part
+
+function display12() { 
+  var content1 =` <h2>All Awesome Books</h2>
+  <div class="contain">
+  </div>`;
+  return content1;
+}
+function display13() {
+  var content1 = `<h2>Add a new book</h2>
+  <div class="form-container">
+  <form  class="form">
+      <input class='book' type="text" name="books" placeholder="Book name"><br />
+      <input class='author' type="text" name="authors" placeholder="Author"><br />
+      <button class="add">Add</button>
+  </form>
+</div>`;
+  return content1;
+}
+function display14() {
+  var content1 = '<h1>This is out contact page.</h1>'
+  return content1;
+}
+
+function getPageContent(page) {
+  var contentToReturn;
+  switch (page) {
+      case 'list':
+          contentToReturn = display12();
+          break;
+      case 'addNew':
+          contentToReturn = display13();
+          break;
+      case 'contact':
+          contentToReturn = display14();
+          break;
+      default:
+          contentToReturn = pages.home;
+          break;
+  }
+  document.getElementById('content').innerHTML =
+      contentToReturn;
+}
